13cbedb5a9f1e771af6a8a52fa66d7f7fb2e4890
Ankur Gangwar <ankurgangwar4575@gmail.com>
2026-08-19 17:16:06 +0530

    Time: 15 ms (89.19%), Space: 60 MB (77.36%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int, vector<int>> mp;
-        for (auto& seat : reservedSeats)
-            mp[seat[0]].push_back(seat[1]);
+        unordered_map<int, int> mp;
+        for (auto& seat : reservedSeats) {
+            if (!mp.count(seat[0]))
+                mp[seat[0]] = 0;
+            mp[seat[0]] |= 1 << seat[1];
+        }
         int ans = (n - mp.size()) * 2;
         for (auto& it : mp) {
-            int mask = 0;
-            for (auto& el : it.second)
-                mask = mask | (1 << el);
+            int mask = it.second;
             if (!(mask & (1 << 2)) && !(mask & (1 << 3)) &&
                 !(mask & (1 << 4)) && !(mask & (1 << 5)) &&
                 !(mask & (1 << 6)) && !(mask & (1 << 7)) &&
@@ -21,8 +22,7 @@ public:
                 ans++;
             else if (!(mask & (1 << 4)) && !(mask & (1 << 5)) &&
                      !(mask & (1 << 6)) && !(mask & (1 << 7)) &&
-                     (mask & (1 << 2) || mask & (1 << 3) || mask & (1 << 8) ||
-                      mask & (1 << 9)))
+                     (mask & (1 << 2) || mask & (1 << 3) || mask & (1 << 8) || mask & (1 << 9)))
                 ans++;
         }
         return ans;
