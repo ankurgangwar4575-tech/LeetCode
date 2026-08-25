fd08261a9314486b41da8fef539fa7e3b1640663
Ankur Gangwar <ankurgangwar4575@gmail.com>
2026-08-25 11:24:59 +0530

    Time: 274 ms (5.71%), Space: 187.6 MB (5.71%) - LeetHub

---
+++ 2411-smallest-subarrays-with-maximum-bitwise-or/2411-smallest-subarrays-with-maximum-bitwise-or.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] smallestSubarrays(int[] nums) {
+        int n = nums.length;
+        long[][] pref = new long[n + 1][32];
+        for (int i = 0; i < n + 1; i++) {
+            for (int b = 0; b < 32; b++)
+                pref[i][b] = 0;
+        }
+        buildPrefixArray(pref, nums);
+        int[] ans = new int[n];
+        Arrays.fill(ans, 0);
+        for (int i = 0; i < n; i++) {
+            int left = i, right = n - 1;
+            long target = bitwiseOr(pref, i, n - 1);
+            int idx = n - 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                long val = bitwiseOr(pref, i, mid);
+                if (val >= target) {
+                    if (val == target)
+                        idx = mid;
+                    right = mid - 1;
+                } else
+                    left = mid + 1;
+            }
+            ans[i] = idx - i + 1;
+        }
+        return ans;
+    }
+
+    private void buildPrefixArray(long[][] pref, int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int b = 0; b < 32; b++) {
+                pref[i + 1][b] = pref[i][b];
+                if ((nums[i]>>b & 1) == 1)
+                    pref[i + 1][b]++;
+            }
+        }
+    }
+
+    private long bitwiseOr(long[][] pref, int l, int r) {
+        long ans = 0;
+        for (int b = 0; b < 32; b++) {
+            long oneBit = pref[r + 1][b] - pref[l][b];
+            if (oneBit > 0)
+                ans |= 1 << b;
+        }
+        return ans;
+    }
+}
